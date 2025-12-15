2ea6a1831235ed5e373de9027dc40d8bb07d945d
Tim Nix <tnix@uttyler.edu>
2025-12-14 21:48:20 -0600

    Commit through Day 12 (all solutions posted)

--- Day 10/problem10b.py
+++ Day 10/problem10b.py
@@ -11,11 +11,15 @@
 # either turns on (if it was off) or turns off (if
 # it was on).
 #
-# Determine the fewest total presses required to
-# correctly configure all indicator lights for all
-# machines in your list. What is the fewest button
-# presses required to correctly configure the
-# indicator lights on all of the machines?
+# Determine the the fewest button presses required
+# to correctly configure the joltage level
+# counters on all of the machines.
+
+import math        # Used for math.inf
+import itertools   # Used for product function
+
+# A dictionary for memoization
+found_states = dict()
 
 # Read in the data file and convert it to a list
 # of strings. 
@@ -56,50 +60,98 @@ def parseInput(values):
       joltages = config[-1][1:-1].split(',')
       joltages = [ int(j) for j in joltages ]
                     
-
-      configs.append((final, wiring, joltages))
+      # Final light scheme is not needed for this.
+      configs.append((tuple(wiring), tuple(joltages)))
    
    return configs
 
 
 
-# Conduct a BFS starting with an initial
-# configuration of all joltages set to zero. Explore
-# using each button press to change the current
-# state. Find the fewest number of button presses
-# needed to reach the joltage configuration.
+# Perform a recursive search for the fewest button
+# pushes to reach the desired joltage state. This
+# is done by determining the button pushes needed
+# to reach the parity joltage, divide resulting
+# joltage by two (for each joltage state) and then
+# recursing on the resulting joltage until all
+# remaining joltages are zero.
 def findFewest(configuration):
-   final, wiring, joltages = configuration
-
-   # Configure initial state (level is the number
-   # of button presses to reach the configuration)
-   states = [(0, [ 0 for i in range(len(joltages)) ])]
-   visited = set()
-
-   # Keep searching until the current state matches
-   # the final state
-   while states[0][1] != joltages:
-      level, current = states.pop(0)
-      # Iterate through each of the button presses
-      for w in wiring:
-         # Configure the updated state
-         next_state = [ s for s in current ]
-         for i in range(len(next_state)):
-            if i in w:
-               next_state[i] += 1
-
-         # Ignore configurations already seen or
-         # if a current joltage is too big and
-         # update the states queue
-         if all([ next_state[i] <= joltages[i] ] for i in range(len(joltages))):
-            t_state = tuple(next_state)
-            if t_state not in visited:
-               visited.add(t_state)
-               states.append((level + 1, next_state))
-
-   # return the 'level' of the found final state
-   return states[0][0]
+   # First check the memoization store.
+   if configuration in found_states:
+      print(found)
+      return found_states[configuration]
    
+   # Break apart the configuration.
+   wiring, final_joltages = configuration
+
+   # If remaining joltages are zero, return zero.
+   if all([ j == 0 for j in final_joltages ]):
+      return 0
+
+   # Determine the light configuration based on
+   # joltage parity
+   final_lights = [ (j % 2) != 0 for j in final_joltages ]   
+
+   # Iterate through all combinations of button
+   # presses (in which each button can be pressed
+   # either once or not at all. Only save those
+   # that lead to final light configuration.
+   possible_presses = list(itertools.product([True, False], repeat=len(wiring)))   
+   presses = list()
+   for sequence in possible_presses:
+      current_lights = [ False for _ in final_joltages ]
+      for s in range(len(sequence)):
+         if sequence[s]:
+            for w in wiring[s]:
+               current_lights[w] = not current_lights[w]
+      if current_lights == final_lights:
+         presses.append(sequence)
+
+   # Iterate through all button press sequences
+   # that lead to the final light configuration.
+   min_presses = math.inf
+   for sequence in presses:
+      # If no combination of button presses exists
+      # to reach the final state, then skip
+      if len(sequence) == 0:
+         total_presses = math.inf
+         
+      # Otherwise, reduce joltages according to
+      # button pushes.
+      else:
+         num_presses = sequence.count(True)
+         next_joltages = [ j for j in final_joltages ]
+         for s in range(len(sequence)):
+            if sequence[s]:
+               for w in wiring[s]:
+                  next_joltages[w] -= 1
+
+         # Skip if any joltage residues are
+         # negative.
+         if all([ j >= 0 for j in next_joltages ]):
+            # All joltages are even; divide by two.
+            if all([ (j % 2) == 0 for j in next_joltages ]):
+               next_joltages = [ j // 2 for j in next_joltages ]
+
+            # Recurse to find remaining joltages.
+            # Total presses are 2x the results of
+            # the recursive call plus the current
+            # number of button pushes.
+            next_config = (wiring, tuple(next_joltages))
+            if next_config in found_states:
+               total_presses = (2 * found_states[next_config]) + num_presses
+            else:
+               total_presses = (2 * findFewest(next_config)) + num_presses
+
+            # Keep track of the minimum number of
+            # button pushes.
+            if total_presses < min_presses:
+               min_presses = total_presses
+
+   # Return the minimum number of button pushes.
+   found_states[configuration] = min_presses
+   return min_presses
+         
+         
 
 if __name__ == '__main__':
    values = readFile("input10b.txt")
@@ -109,11 +161,11 @@ if __name__ == '__main__':
    # configuration, find the fewest button presses
    # to reach the final state and increment count
    button_presses = 0
-   for c in range(len(configs)):
-      print(c)
-      button_presses += findFewest(configs[c])
+   for c in configs:
+      button_presses += findFewest(c)
 
    # Display result
    print('total button presses = ' + str(button_presses))
-   
+
+   # Received help from Reddit on this one.
    
